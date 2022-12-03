e2b4b8e051cd01b61476c42624ed8f397139d31f
KevinAryel <kevinaryel.dev@gmail.com>
2022-12-03 15:57:15 -0300

    feat: foto e processo data

---
+++ testes-aceitacao-contrato/src/main/java/br/com/dbccompany/chronos/data/changeless/FotoData.java
@@ -0,0 +1,6 @@
+package br.com.dbccompany.chronos.data.changeless;
+
+public class FotoData {
+    public static final String SERVICE = "/foto";
+    public static final String SERVICE_UPLOAD_SELF = String.format("%s/upload-image-perfil", SERVICE);
+}
---
+++ testes-aceitacao-contrato/src/main/java/br/com/dbccompany/chronos/data/changeless/ProcessoData.java
@@ -0,0 +1,5 @@
+package br.com.dbccompany.chronos.data.changeless;
+
+public class ProcessoData {
+    public static final String SERVICE = "/processo";
+}
