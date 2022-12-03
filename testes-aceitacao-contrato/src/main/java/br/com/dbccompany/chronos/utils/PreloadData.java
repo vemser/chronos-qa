fc58e4b2dbc4059a6cf66bbec9f17d5af710dc9f
KevinAryel <kevinaryel.dev@gmail.com>
2022-12-03 15:52:15 -0300

    feat: classe de preload e ajustes nos logins

--- testes-aceitacao-contrato/src/main/java/br/com/dbccompany/chronos/utils/PreloadData.java
+++ testes-aceitacao-contrato/src/main/java/br/com/dbccompany/chronos/utils/PreloadData.java
@@ -1,28 +1,55 @@
 package br.com.dbccompany.chronos.utils;
 
-import br.com.dbccompany.chronos.client.AreaEnvolvidaClient;
-import br.com.dbccompany.chronos.client.ResponsavelClient;
-import br.com.dbccompany.chronos.data.factory.AreaEnvolvidaDataFactory;
-import br.com.dbccompany.chronos.data.factory.ResponsavelDataFactory;
-import br.com.dbccompany.chronos.dto.AreaEnvolvidaDTO;
-import br.com.dbccompany.chronos.dto.ResponsavelDTO;
-import br.com.dbccompany.chronos.model.AreaEnvolvida;
-import br.com.dbccompany.chronos.model.Responsavel;
+import br.com.dbccompany.chronos.client.*;
+import br.com.dbccompany.chronos.data.factory.*;
+import br.com.dbccompany.chronos.dto.*;
+import br.com.dbccompany.chronos.model.*;
 import br.com.dbccompany.chronos.utils.Utils;
 
 public class PreloadData {
+    public static UsuarioDTO userAdmin(){
+        User usuario = UserDataFactory.usuarioValido();
+        UsuarioDTO userAdmin = UsuarioClient.cadastrarUsuario(Utils.converterParaJson(usuario),true)
+            .then()
+            .extract().as(UsuarioDTO.class);
+        return userAdmin;
+    }
     public static ResponsavelDTO responsavel(){
         Responsavel responsavel = ResponsavelDataFactory.responsavelValido();
         return ResponsavelClient.criarResponsavel(Utils.converterParaJson(responsavel), true)
-                .then()
-                .log().all()
-                .extract().as(ResponsavelDTO.class);
+            .then()
+            .log().all()
+            .extract().as(ResponsavelDTO.class);
     }
     public static AreaEnvolvidaDTO areaEnvolvida(){
         AreaEnvolvida areaEnvolvida = AreaEnvolvidaDataFactory.areaEnvolvidaValida();
         return AreaEnvolvidaClient.criarAreaEnvolvida(Utils.converterParaJson(areaEnvolvida), true)
+            .then()
+            .log().all()
+            .extract().as(AreaEnvolvidaDTO.class);
+    }
+    public static ProcessoDTO processo(String idEtapa){
+        Processo processo = ProcessoDataFactory.processoValido();
+        return ProcessoClient.cadastrarProcesso(Utils.converterParaJson(processo), idEtapa, true)
+            .then()
+            .extract().as(ProcessoDTO.class);
+    }
+    public static EdicaoDTO edicao(){
+        Edicao edicao = EdicaoDataFactory.edicaoValida();
+        return EdicaoClient.cadastrarEdicao(Utils.converterParaJson(edicao),true)
+            .then()
+            .extract().as(EdicaoDTO.class);
+    }
+    public static EtapaDTO etapa(String idEdicao){
+        Etapa etapa = EtapaDataFactory.etapaValida();
+        return EtapaClient.criarEtapa(Utils.converterParaJson(etapa),idEdicao,true)
                 .then()
-                .log().all()
-                .extract().as(AreaEnvolvidaDTO.class);
+                .extract().as(EtapaDTO.class);
+    }
+    public static DiaNaoUtilDTO diaNaoUtil(){
+        DiaNaoUtil diaNaoUtil = DiaNaoUtilDataFactory.diaNaoUtilValido();
+        return DiaNaoUtilClient.criarDiaNaoUtil(Utils.converterParaJson(diaNaoUtil),true)
+                .then()
+                .extract().as(DiaNaoUtilDTO.class);
     }
 }
