2202c7dfb275dd4f7808369459adf2e40aeac980
KevinAryel <kevinaryel.dev@gmail.com>
2022-12-04 20:58:38 -0300

    feat: teste de processo

---
+++ front-aceitacao-e2e/src/test/java/br/com/dbccompany/chronos/steps/TesteProcessoSteps.java
@@ -0,0 +1,115 @@
+package br.com.dbccompany.chronos.steps;
+
+import br.com.dbccompany.chronos.pages.CadastrarEditarProcessoPage;
+import br.com.dbccompany.chronos.pages.EdicoesPage;
+import br.com.dbccompany.chronos.pages.VerificarEdicaoPage;
+import br.com.dbccompany.chronos.utils.Utils;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.Test;
+
+public class TesteProcessoSteps extends BaseSteps {
+
+    private static void criarProcessoValido(){
+        TesteEtapaSteps.criarEtapaValida();
+        VerificarEdicaoPage.clicarBotaoAdicionarProcesso();
+        CadastrarEditarProcessoPage.preencherNome(Utils.faker.lorem().characters(10));
+        CadastrarEditarProcessoPage.selecionarOpcaoAreaEnvolvida();
+        CadastrarEditarProcessoPage.selecionarOpcaoResponsavel();
+        CadastrarEditarProcessoPage.preencherDuracao("10");
+        CadastrarEditarProcessoPage.preencherDiasUteis("5");
+        CadastrarEditarProcessoPage.preencherOrdem("1");
+        CadastrarEditarProcessoPage.clicarBotaoSubmit();
+
+    }
+    @Test
+    @Owner("Kevin Aryel")
+    @Epic("Frontend")
+    @Feature("Processo")
+    @Story("Criar Processo")
+    public void testarCriarProcessoComSucesso() throws InterruptedException {
+        fazerLoginGestor();
+        entrarNaPaginaDeEdicoes();
+        TesteEdicoesSteps.criarEdicaoValida();
+        try{
+            EdicoesPage.clicarBotaoDetalhesUltima();
+            criarProcessoValido();
+            CadastrarEditarProcessoPage.validarToastSucesso();
+        } finally {
+            entrarNaPaginaDeEdicoes();
+            EdicoesPage.clicarBotaoExcluirUltima();
+        }
+    }
+    @Test
+    @Owner("Kevin Aryel")
+    @Epic("Frontend")
+    @Feature("Processo")
+    @Story("Criar Processo")
+    public void testarCriarProcessoComNovoResponsavelAreaEnvolvida() throws InterruptedException {
+        fazerLoginGestor();
+        entrarNaPaginaDeEdicoes();
+        TesteEdicoesSteps.criarEdicaoValida();
+        try{
+            EdicoesPage.clicarBotaoDetalhesUltima();
+            TesteEtapaSteps.criarEtapaValida();
+            VerificarEdicaoPage.clicarBotaoAdicionarProcesso();
+            CadastrarEditarProcessoPage.preencherNome(Utils.faker.lorem().characters(10));
+            CadastrarEditarProcessoPage.preencherAreaEnvolvida(Utils.faker.job().position());
+            CadastrarEditarProcessoPage.preencherResponsavel(Utils.faker.job().title());
+            CadastrarEditarProcessoPage.preencherDuracao("10");
+            CadastrarEditarProcessoPage.preencherDiasUteis("5");
+            CadastrarEditarProcessoPage.preencherOrdem("1");
+            CadastrarEditarProcessoPage.clicarBotaoSubmit();
+            CadastrarEditarProcessoPage.validarToastSucesso();
+        } finally {
+            entrarNaPaginaDeEdicoes();
+            EdicoesPage.clicarBotaoExcluirUltima();
+        }
+    }
+
+    @Test
+    @Owner("Kevin Aryel")
+    @Epic("Frontend")
+    @Feature("Processo")
+    @Story("Editar Processo")
+    public void testarEditarProcessoComSucesso() throws InterruptedException{
+        fazerLoginGestor();
+        entrarNaPaginaDeEdicoes();
+        TesteEdicoesSteps.criarEdicaoValida();
+        try {
+            EdicoesPage.clicarBotaoDetalhesUltima();
+            criarProcessoValido();
+            VerificarEdicaoPage.clicarBotaoEditarProcesso();
+            CadastrarEditarProcessoPage.preencherNome(Utils.faker.lorem().characters(10));
+            CadastrarEditarProcessoPage.preencherDuracao(Utils.faker.number().digits(1));
+            CadastrarEditarProcessoPage.preencherDiasUteis("5");
+            CadastrarEditarProcessoPage.preencherOrdem(Utils.faker.number().digits(2));
+            CadastrarEditarProcessoPage.clicarBotaoSubmit();
+            CadastrarEditarProcessoPage.validarToastSucesso();
+        } finally {
+            entrarNaPaginaDeEdicoes();
+            EdicoesPage.clicarBotaoExcluirUltima();
+        }
+    }
+    @Test
+    @Owner("Kevin Aryel")
+    @Epic("Frontend")
+    @Feature("Processo")
+    @Story("Deletar Processo")
+    public void testarDeletarProcessoComSucesso() throws InterruptedException{
+        fazerLoginGestor();
+        entrarNaPaginaDeEdicoes();
+        TesteEdicoesSteps.criarEdicaoValida();
+        try {
+            EdicoesPage.clicarBotaoDetalhesUltima();
+            criarProcessoValido();
+            VerificarEdicaoPage.clicarBotaoExcluirProcesso();
+            CadastrarEditarProcessoPage.validarToastSucesso();
+        } finally {
+            entrarNaPaginaDeEdicoes();
+            EdicoesPage.clicarBotaoExcluirUltima();
+        }
+    }
+}
