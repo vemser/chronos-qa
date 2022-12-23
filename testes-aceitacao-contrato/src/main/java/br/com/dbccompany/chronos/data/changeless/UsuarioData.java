cc39099d589a29d0ae7d1637218f0fb0d90c419b
Kevin <kevinaryel.dev@gmail.com>
2022-12-23 16:22:26 -0300

    fix: initialize error corrigido

--- testes-aceitacao-contrato/src/main/java/br/com/dbccompany/chronos/data/changeless/UsuarioData.java
+++ testes-aceitacao-contrato/src/main/java/br/com/dbccompany/chronos/data/changeless/UsuarioData.java
@@ -4,9 +4,9 @@ public class UsuarioData {
 
         private UsuarioData() {}
         private static final String BaseURI = "http://vemser-dbc.dbccompany.com.br:39000/vemser/usuario-back";
-        public static final String SERVICE = String.format("%s/usuario", BaseURI);
+        public static final String SERVICE = String.format("%s/usuario",BaseURI);
         public static final String SERVICE_UPLOAD = String.format("%s/foto/upload-image", BaseURI);
         public static final String SERVICE_UPDATE_CARGO = String.format("%s/update-cargos", SERVICE);
         public static final String SERVICE_LOGADO = String.format("%s/logged-user", SERVICE);
-        public static final String SERVICE_FILTRADO = String.format("%s/filtrarLoginCargo");
+        public static final String SERVICE_FILTRADO = String.format("%s/filtrarLoginCargo",SERVICE);
 }
