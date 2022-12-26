602ac128ca332aa99d07eb2749ef89d02d6bbc37
KevinAryel <kevinaryel.dev@gmail.com>
2022-12-26 14:55:39 -0300

    fix: testes de processo arrumados

--- testes-aceitacao-contrato/src/test/java/br/com/dbccompany/chronos/testes/aceitacao/processo/ProcessoEditarTest.java
+++ testes-aceitacao-contrato/src/test/java/br/com/dbccompany/chronos/testes/aceitacao/processo/ProcessoEditarTest.java
@@ -12,6 +12,7 @@ import br.com.dbccompany.chronos.testes.BaseTest;
 import br.com.dbccompany.chronos.utils.PreloadData;
 import br.com.dbccompany.chronos.utils.Utils;
 import io.qameta.allure.*;
+import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.Assert;
 import org.junit.jupiter.api.Tag;
@@ -116,11 +117,11 @@ public class ProcessoEditarTest extends BaseTest {
             ProcessoDTO processo = PreloadData.processo(idEtapa);
             String idProcesso = processo.getIdProcesso().toString();
             Processo processoEditar = ProcessoDataFactory.processoSemProcessoCritico();
-            ResponseErrorBadDTO response = ProcessoClient.atualizarProcesso(Utils.converterParaJson(processoEditar),idProcesso,true)
+            Response response = ProcessoClient.atualizarProcesso(Utils.converterParaJson(processoEditar),idProcesso,true)
                     .then()
                     .log().all()
-                    .extract().as(ResponseErrorBadDTO.class);
-            Assert.assertEquals(HttpStatus.SC_BAD_REQUEST, response.getStatus().intValue());
+                    .extract().response();
+            Assert.assertEquals(HttpStatus.SC_BAD_REQUEST, response.getStatusCode());
         } finally {
             EdicaoClient.deletarEdicao(idEdicao,true);
         }
@@ -143,11 +144,11 @@ public class ProcessoEditarTest extends BaseTest {
             ProcessoDTO processo = PreloadData.processo(idEtapa);
             String idProcesso = processo.getIdProcesso().toString();
             Processo processoEditar = ProcessoDataFactory.processoComProcessoCriticoInvalido();
-            ResponseErrorBadDTO response = ProcessoClient.atualizarProcesso(Utils.converterParaJson(processoEditar),idProcesso,true)
+            Response response = ProcessoClient.atualizarProcesso(Utils.converterParaJson(processoEditar),idProcesso,true)
                     .then()
                     .log().all()
-                    .extract().as(ResponseErrorBadDTO.class);
-            Assert.assertEquals(HttpStatus.SC_BAD_REQUEST, response.getStatus().intValue());
+                    .extract().response();
+            Assert.assertEquals(HttpStatus.SC_BAD_REQUEST, response.getStatusCode());
         } finally {
             EdicaoClient.deletarEdicao(idEdicao,true);
         }
